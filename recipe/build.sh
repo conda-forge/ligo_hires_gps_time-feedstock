6bcabbeae6aa630911c4b064dc58078a3ebcdbc4
Erik von Reis <evonreis@caltech.edu>
2025-08-06 10:29:54 -0700

    MNT: Re-rendered with conda-build 25.7.0, conda-smithy 3.51.1, and conda-forge-pinning 2025.08.04.23.25.35

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e -x
 
+echo $CONDA_PREFIX
+echo $CONDA_BUILD_SYSROOT
+echo $LDFLAGS
+
 cargo run --bin stub_gen --features hifitime,python
 
 $PYTHON -m pip install ./ligo_hires_gps_time -vv
