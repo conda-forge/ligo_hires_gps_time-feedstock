b6df7c3a90a13705ed91b17a4c46fffe4dd94aa6
Erik von Reis <evonreis@caltech.edu>
2025-08-06 10:47:38 -0700

    drop c-compiler for windows
    don't try to build stub during conda build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,6 @@ echo $CONDA_PREFIX
 echo $CONDA_BUILD_SYSROOT
 echo $LDFLAGS
 
-cargo run --bin stub_gen --features hifitime,python
+# cargo run --bin stub_gen --features hifitime,python
 
 $PYTHON -m pip install ./ligo_hires_gps_time -vv
